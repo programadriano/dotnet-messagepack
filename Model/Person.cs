5728ded11520ef19df5466a1e3a4d21bc4a88476
Thiago S Adriano <tadriano.net@gmail.com>
2023-09-23 17:44:41 -0300

    atualizando exemplo

---
+++ Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using MessagePack;
+
+namespace dotnet_messagepack.Model;
+
+[MessagePackObject]
+public class Pessoa
+{
+    [Key("nome")]
+    public string Nome { get; set; }
+
+    [Key("idade")]
+    public int Idade { get; set; }
+
+    [Key("cidade")]
+    public string Cidade { get; set; }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dotnet_messagepack.Model;
+using MessagePack;
+
+using (HttpClient client = new())
+{
+    string url = "http://localhost:3000"; 
+
+    try
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            Stream stream = await response.Content.ReadAsStreamAsync();
+
+            var decodedData = MessagePackSerializer.Deserialize<Pessoa>(stream);
+
+            Console.WriteLine("Dados decodificados em C#:");
+            Console.WriteLine($"Nome: {decodedData.Nome}");
+            Console.WriteLine($"Idade: {decodedData.Idade}");
+            Console.WriteLine($"Cidade: {decodedData.Cidade}");
+        }
+        else
+        {
+            Console.WriteLine($"Erro: {response.StatusCode}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}");
+    }
+}
+
+Console.ReadLine();
